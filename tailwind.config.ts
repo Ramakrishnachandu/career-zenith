fb5bda9b71fe6a667864028e3fbd01fa702bf1ff
CHANDU VEERA RAMAKRISHNA <ramakrishnachandu43@gmail.com>
2024-08-15 23:59:35 +0530

    updated code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/UIPages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/UIcomponents/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-
       boxShadow: {
-        "hover": "0 6px 12px #cbd5e1",
-        "active": "0 6px 12px #e2e8f0",
-        'card': " 0px 14px 40px var(--Ev400, rgba(30, 10, 58, 0.1));"
-      }
+        hover: "0 6px 12px #cbd5e1",
+        active: "0 6px 12px #e2e8f0",
+        card: "0px 14px 40px var(--Ev400, rgba(30, 10, 58, 0.1))",
+      },
     },
   },
   plugins: [],
