688a889f817c91a1b68bcf75a9494ae9d23431f4
CHANDU VEERA RAMAKRISHNA <ramakrishnachandu43@gmail.com>
2024-08-20 00:17:49 +0530

    added developer profile to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-// app/layout.tsx (or layout.js)
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./not-found";
 import Header from "@/UIComponents/layout/Header";
 import ToastContainer from "@/UIComponents/toast/ToastWrapper";
+import Head from "next/head";
+import Footer from "@/UIComponents/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +14,13 @@ export const metadata: Metadata = {
   title: "Career Zenith - Your Gateway to Job Opportunities",
   description:
     "Career Zenith is designed to help job seekers find their ideal roles and allow employers to post job openings seamlessly.",
-  authors: [{ name: "Veera Ramakrishna Chandu" }],
+  authors: [{ name: "Veera Ramakrishna Chandu", url: "https://career-zenith.vercel.app/developer-profile" }],
   applicationName: "Career Zenith",
   keywords:
     "job portal, job search, career opportunities, employment services, job listings, job vacancies, recruitment platform, career zenith",
   openGraph: {
     title: "Career Zenith - Your Gateway to Job Opportunities",
-    description:
-      "Discover job opportunities and connect with employers on Career Zenith.",
+    description: "Discover job opportunities and connect with employers on Career Zenith.",
     url: "https://career-zenith.vercel.app",
     siteName: "Career Zenith",
     images: [
@@ -31,15 +32,14 @@ export const metadata: Metadata = {
       },
     ],
   },
-  other: {
-    'google-site-verification': '6AgdzYlcmn57DL4KSgFa4f3SYXZkesyJ8hVC1kdcOWw',
-  },
   twitter: {
     card: "summary_large_image",
     title: "Career Zenith - Your Gateway to Job Opportunities",
-    description:
-      "Discover job opportunities and connect with employers on Career Zenith.",
+    description: "Discover job opportunities and connect with employers on Career Zenith.",
     images: "/logo.svg",
+  },
+  other: {
+    'google-site-verification': '6AgdzYlcmn57DL4KSgFa4f3SYXZkesyJ8hVC1kdcOWw',
   },
 };
 
@@ -50,11 +50,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={inter.className}>
         <div>
-          <Header />
           <ToastContainer />
+          <Header />
           {children}
+          <Footer />
         </div>
       </body>
     </html>
