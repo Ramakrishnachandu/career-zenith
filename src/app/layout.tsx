248b2860e9b283a5ec76b187255b52e02d6be801
CHANDU VEERA RAMAKRISHNA <ramakrishnachandu43@gmail.com>
2024-08-19 09:38:51 +0530

    added google meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import './not-found';
+import "./not-found";
 import Header from "@/UIComponents/layout/Header";
 import ToastContainer from "@/UIComponents/toast/ToastWrapper";
 
@@ -10,13 +10,16 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Career Zenith - Your Gateway to Job Opportunities",
-  description: "Career Zenith is designed to help job seekers find their ideal roles and allow employers to post job openings seamlessly.",
-  authors: [{ name: 'Veera Ramakrishna Chandu' }],
-  applicationName: 'Career Zenith',
-  keywords: 'job portal, job search, career opportunities, employment services, job listings, job vacancies, recruitment platform',
+  description:
+    "Career Zenith is designed to help job seekers find their ideal roles and allow employers to post job openings seamlessly.",
+  authors: [{ name: "Veera Ramakrishna Chandu" }],
+  applicationName: "Career Zenith",
+  keywords:
+    "job portal, job search, career opportunities, employment services, job listings, job vacancies, recruitment platform, career zenith",
   openGraph: {
     title: "Career Zenith - Your Gateway to Job Opportunities",
-    description: "Discover job opportunities and connect with employers on Career Zenith.",
+    description:
+      "Discover job opportunities and connect with employers on Career Zenith.",
     url: "https://career-zenith.vercel.app",
     siteName: "Career Zenith",
     images: [
@@ -28,14 +31,17 @@ export const metadata: Metadata = {
       },
     ],
   },
+  other: {
+    'google-site-verification': '6AgdzYlcmn57DL4KSgFa4f3SYXZkesyJ8hVC1kdcOWw',
+  },
   twitter: {
     card: "summary_large_image",
     title: "Career Zenith - Your Gateway to Job Opportunities",
-    description: "Discover job opportunities and connect with employers on Career Zenith.",
+    description:
+      "Discover job opportunities and connect with employers on Career Zenith.",
     images: "/logo.svg",
   },
 };
-
 
 export default function RootLayout({
   children,
